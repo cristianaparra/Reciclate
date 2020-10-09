f1ab98cb197a9006a2755726ca67b4b31fd79192
Cristian Parra <cristian.a.parra@gmail.com>
2020-10-09 20:02:44 -0300

    pasando paso a paso  a un componente llamdo step que se visualiza en home

--- src/js/Components/Step.js
+++ src/js/Components/Step.js
@@ -3,26 +3,27 @@ import React from 'react';
 const Step = () => {
     return (
         <>
+            <h1>¿Como reciclar? ¡Es fácil!  <i class="fas fa-recycle" /></h1>
             <div className="card mb-3">
-                <img src="..." className="card-img-top" alt="..." />
+                <img src="..." className="card-img-left" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
+
+                  <h5 className="card-title">De manera simple registra tu basura </h5>
+                    <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+            <div className="card mb-3">
+                <img src="..." className="card-img-rigth" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Recolecta o busca un reciclador en nuestra lista</h5>
                     <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                 </div>
             </div>
             <div className="card mb-3">
-                <img src="..." className="card-img-top" alt="..." />
+                <img src="..." className="card-img-left" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-            </div>
-            <div className="card mb-3">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
+                    <h5 className="card-title">Registrarte como recolector para recibir basura!</h5>
                     <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                 </div>
